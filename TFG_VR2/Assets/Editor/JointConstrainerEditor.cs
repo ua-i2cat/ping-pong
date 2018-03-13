bb5ec954d547200378bb36eb52dac0c3b9465ff9
Alex Via <alexandre.via@i2cat.net>
2018-03-13 13:02:29 +0100

    TFG_VR2 Part3

---
+++ TFG_VR2/Assets/Editor/JointConstrainerEditor.cs
@@ -0,0 +1,100 @@
+﻿// Licensed under the LGPL 3.0
+// See the LICENSE file in the project root for more information.
+// Author: alexandre.via@i2cat.net
+
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(JointConstrainer)), CanEditMultipleObjects]
+public class JointConstrainerEditor : Editor
+{
+    public SerializedProperty
+        constrainerType_Prop,
+        active_Prop,
+        drawProjection_Prop,
+        minAngle_Prop,
+        maxAngle_Prop,
+        transform_Prop,
+        parent_Prop,
+        child_Prop,
+        plane_Prop,
+        rotAxis_Prop,
+        threshold_Prop,
+        mag_Prop,
+        localForward_Prop;
+
+    void OnEnable()
+    {
+        // Setup the SerializedProperties
+        constrainerType_Prop = serializedObject.FindProperty("constrainerType");
+        active_Prop = serializedObject.FindProperty("active");
+        drawProjection_Prop = serializedObject.FindProperty("drawProjection");
+        minAngle_Prop = serializedObject.FindProperty("minAngle");
+        maxAngle_Prop = serializedObject.FindProperty("maxAngle");
+        transform_Prop = serializedObject.FindProperty("transform");
+        transform_Prop = serializedObject.FindProperty("transform");
+        parent_Prop = serializedObject.FindProperty("parent");
+        child_Prop = serializedObject.FindProperty("child");
+        plane_Prop = serializedObject.FindProperty("plane");
+        rotAxis_Prop = serializedObject.FindProperty("rotAxis");
+        threshold_Prop = serializedObject.FindProperty("threshold");
+        mag_Prop = serializedObject.FindProperty("mag");
+        localForward_Prop = serializedObject.FindProperty("localForward");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        EditorGUILayout.PropertyField(constrainerType_Prop);
+        EditorGUILayout.PropertyField(active_Prop, new GUIContent("active"));
+        EditorGUILayout.ObjectField(transform_Prop, new GUIContent("transform"));
+
+        JointConstrainer.ConstrainerType type = (JointConstrainer.ConstrainerType)constrainerType_Prop.enumValueIndex;
+
+        switch (type)
+        {
+            case JointConstrainer.ConstrainerType.Constrainer_angle:
+                EditorGUILayout.PropertyField(maxAngle_Prop, new GUIContent("maxAngle"));
+                EditorGUILayout.ObjectField(parent_Prop, new GUIContent("parent"));
+                EditorGUILayout.ObjectField(child_Prop, new GUIContent("child"));
+                break;
+
+            case JointConstrainer.ConstrainerType.Constrainer_minmaxangle:
+                EditorGUILayout.PropertyField(minAngle_Prop, new GUIContent("minAngle"));
+                EditorGUILayout.PropertyField(maxAngle_Prop, new GUIContent("maxAngle"));
+                EditorGUILayout.ObjectField(parent_Prop, new GUIContent("parent"));
+                EditorGUILayout.ObjectField(child_Prop, new GUIContent("child"));
+                break;
+
+            case JointConstrainer.ConstrainerType.Constrainer_minmaxangle_plane:
+                EditorGUILayout.PropertyField(drawProjection_Prop, new GUIContent("drawProjection"));
+                EditorGUILayout.PropertyField(minAngle_Prop, new GUIContent("minAngle"));
+                EditorGUILayout.PropertyField(maxAngle_Prop, new GUIContent("maxAngle"));
+                EditorGUILayout.ObjectField(parent_Prop, new GUIContent("parent"));
+                EditorGUILayout.ObjectField(child_Prop, new GUIContent("child"));
+                EditorGUILayout.ObjectField(plane_Prop, new GUIContent("plane"));
+                EditorGUILayout.PropertyField(threshold_Prop, new GUIContent("threshold"));
+                EditorGUILayout.PropertyField(mag_Prop, new GUIContent("mag"));
+                break;           
+
+            case JointConstrainer.ConstrainerType.Constrainer_plane:
+                EditorGUILayout.PropertyField(drawProjection_Prop, new GUIContent("drawProjection"));
+                EditorGUILayout.ObjectField(plane_Prop, new GUIContent("plane"));
+                EditorGUILayout.ObjectField(parent_Prop, new GUIContent("parent"));
+                EditorGUILayout.ObjectField(child_Prop, new GUIContent("child"));
+                EditorGUILayout.PropertyField(threshold_Prop, new GUIContent("threshold"));
+                EditorGUILayout.PropertyField(mag_Prop, new GUIContent("mag"));
+                break;
+
+            case JointConstrainer.ConstrainerType.Constrainer_twist:
+                EditorGUILayout.PropertyField(minAngle_Prop, new GUIContent("minAngle"));
+                EditorGUILayout.PropertyField(maxAngle_Prop, new GUIContent("maxAngle"));
+                EditorGUILayout.PropertyField(localForward_Prop);
+                break;
+        }
+
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
---
+++ TFG_VR2/Assets/Editor/SolidArcEditor.cs
@@ -0,0 +1,110 @@
+﻿// Licensed under the LGPL 3.0
+// See the LICENSE file in the project root for more information.
+// Author: alexandre.via@i2cat.net
+
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(UnityJoint)), CanEditMultipleObjects]
+public class SolidArcEditor : Editor
+{
+    void OnSceneGUI()
+    {
+        UnityJoint joint = target as UnityJoint;
+
+        if (joint.transform.childCount == 0)
+            return;
+        Transform child = joint.transform.GetChild(0);
+        Vector3 forward = (child.position - joint.transform.position).normalized;
+        float radius = Vector3.Distance(child.position, joint.transform.position);
+        Quaternion rot = Quaternion.Euler(0, -Mathf.Abs(joint.MaxAngleCCW), 0);
+
+        SolidArc t = new SolidArc();
+        t.from = rot * forward;
+        t.angle = joint.MaxAngleCW + joint.MaxAngleCCW;
+
+        if (joint.YAxisRotation == UnityJoint.AxisRotState_Enum.Constrained)
+        {
+            Handles.color = new Color(0, 1, 0, 0.2f);
+            Vector3 from = Quaternion.Euler(0, -joint.YMinAngle, 0) * forward;
+            float angle = joint.YMinAngle + joint.YMaxAngle;
+            Handles.DrawSolidArc(joint.transform.position,
+                Vector3.up, from, angle, radius);
+        }
+
+        if (joint.XAxisRotation == UnityJoint.AxisRotState_Enum.Constrained)
+        {
+            Handles.color = new Color(1, 0, 0, 0.2f);
+            Vector3 from = Quaternion.Euler(-joint.XMinAngle, 0, 0) * Vector3.up;
+            float angle = joint.XMinAngle + joint.XMaxAngle;
+            Handles.DrawSolidArc(joint.transform.position,
+                Vector3.right, from, angle, radius);
+        }
+
+        if (joint.ZAxisRotation == UnityJoint.AxisRotState_Enum.Constrained)
+        { 
+            Handles.color = new Color(0, 0, 1, 0.2f);
+            Vector3 from = Quaternion.Euler(0, 0, -joint.ZMinAngle) * forward;
+            float angle = joint.ZMinAngle + joint.ZMaxAngle;
+            Handles.DrawSolidArc(joint.transform.position,
+                Vector3.forward, from, angle, radius);
+        }
+
+        Handles.color = Color.magenta;
+        Handles.DrawAAPolyLine(5, new Vector3[] { joint.transform.position, child.position });
+
+        Handles.color = Color.white;
+    }
+
+    public SerializedProperty
+        XAxisRotation_Prop,
+        YAxisRotation_Prop,
+        ZAxisRotation_Prop,
+        XMinAngle_Prop,
+        XMaxAngle_Prop,
+        YMinAngle_Prop,
+        YMaxAngle_Prop,
+        ZMinAngle_Prop,
+        ZMaxAngle_Prop;
+
+    void OnEnable()
+    {
+        XAxisRotation_Prop = serializedObject.FindProperty("XAxisRotation");
+        YAxisRotation_Prop = serializedObject.FindProperty("YAxisRotation");
+        ZAxisRotation_Prop = serializedObject.FindProperty("ZAxisRotation");
+        XMinAngle_Prop = serializedObject.FindProperty("XMinAngle");
+        XMaxAngle_Prop = serializedObject.FindProperty("XMaxAngle");
+        YMinAngle_Prop = serializedObject.FindProperty("YMinAngle");
+        YMaxAngle_Prop = serializedObject.FindProperty("YMaxAngle");
+        ZMinAngle_Prop = serializedObject.FindProperty("ZMinAngle");
+        ZMaxAngle_Prop = serializedObject.FindProperty("ZMaxAngle");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        EditorGUILayout.PropertyField(XAxisRotation_Prop);
+        if(XAxisRotation_Prop.enumValueIndex == 1)
+        {
+            EditorGUILayout.PropertyField(XMinAngle_Prop, new GUIContent("XMinAngle"));
+            EditorGUILayout.PropertyField(XMaxAngle_Prop, new GUIContent("XMaxAngle"));
+        }
+
+        EditorGUILayout.PropertyField(YAxisRotation_Prop);
+        if (YAxisRotation_Prop.enumValueIndex == 1)
+        {
+            EditorGUILayout.PropertyField(YMinAngle_Prop, new GUIContent("YMinAngle"));
+            EditorGUILayout.PropertyField(YMaxAngle_Prop, new GUIContent("YMaxAngle"));
+        }
+
+        EditorGUILayout.PropertyField(ZAxisRotation_Prop);
+        if (ZAxisRotation_Prop.enumValueIndex == 1)
+        {
+            EditorGUILayout.PropertyField(ZMinAngle_Prop, new GUIContent("ZMinAngle"));
+            EditorGUILayout.PropertyField(ZMaxAngle_Prop, new GUIContent("ZMaxAngle"));
+        }
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
