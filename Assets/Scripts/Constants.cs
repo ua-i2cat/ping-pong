32e55ffe7b08ba429f56270c27a28b590dd68e67
Alex Via <alexandre.via@i2cat.net>
2018-03-23 13:44:53 +0100

    Added Body and Rig constants

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -7,6 +7,8 @@ public static class Constants
     public static string IP = "147.83.206.67";
     public static int PORT = 33333;
 
+    public static string Body = "Body";
+    public static string Rig = "Rig";
     public static string Eye = "Eye";
     public static string LeftHand = "LH";
     public static string RightHand = "RH";
