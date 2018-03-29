74696a206558732184b202fb79143b2bd561e6c6
Alex Via <alexandre.via@i2cat.net>
2018-03-29 10:28:15 +0200

    Created script to serve the ball on the BouncePhysics scene

---
+++ Assets/ServeBall.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SteamVR_TrackedController))]
+public class ServeBall : MonoBehaviour
+{
+    private SteamVR_TrackedController inputController;
+
+    private void Start()
+    {
+        inputController = GetComponent<SteamVR_TrackedController>();
+        inputController.TriggerClicked += OnTriggerClicked;
+    }
+
+    private void OnTriggerClicked(object sender, ClickedEventArgs e)
+    {
+        //Debug.Log("Trigger Pressed");
+        BallController ballController = GameObject.Find("Ball").GetComponent<BallController>();
+        ballController.paddle = transform.Find("attach").Find("Collider").gameObject;
+        ballController.serve = true;
+    }  
+}
