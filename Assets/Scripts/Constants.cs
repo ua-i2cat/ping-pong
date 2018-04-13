c7a35857385996685a70716e79055edb19847670
Alex Via <alexandre.via@i2cat.net>
2018-04-13 13:06:21 +0200

    Added some constants

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -2,6 +2,8 @@
 // See the LICENSE file in the project root for more information.
 // Author: alexandre.via@i2cat.net
 
+using UnityEngine;
+
 public static class Constants
 {
     public static string IP = "147.83.206.67";
@@ -21,7 +23,7 @@ public static class Constants
     public static string RightFoot = "RF";
 
     public static string ServeRequest = "ServeRequest";
-    public static string WelcomeMsg = "Welcome\n";
+    public static string WelcomeMsg = "Welcome";
 
     public static string OnlineText = "Online";
     public static string OfflineText = "Offline";
@@ -29,4 +31,10 @@ public static class Constants
     public static string SendInputField = "SendInputField";
 
     public static string Ball = "Ball";
+
+    public static Vector3 RightHandScale = new Vector3(0.11692f, 0.11692f, 0.11692f);
+    public static Vector3 ColliderCenter = new Vector3(0, -0.15f, 1);
+    public static Vector3 ColliderSize = new Vector3(2, 0.5f, 3);
+
+    public static Vector3 SphereScale = new Vector3(0.1f, 0.1f, 0.1f);
 }
