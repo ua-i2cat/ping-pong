457cd9822602e1172efe188fdcfb5969864a0705
Alex Via <alexandre.via@i2cat.net>
2018-04-20 08:25:21 +0200

    New ServerManagerUDP using the framework

---
+++ Assets/Scripts/Pilot1_ServerManager.cs
@@ -0,0 +1,45 @@
+﻿// Licensed under the LGPL 3.0
+// See the LICENSE file in the project root for more information.
+// Author: alexandre.via@i2cat.net
+
+using System.Text;
+using UnityEngine;
+
+public class Pilot1_ServerManager : MonoBehaviour
+{
+    private Server server = new ServerTCP();
+    private int port = 44444;
+
+    //private SessionManager sessionManager = new SessionManager();
+    //private StreamingManager streamingManager = new StreamingManager();
+    //private ConnectionManager connectionManager = new ConnectionManager();
+    //private Orchestrator orchestrator = new Orchestrator();
+
+    private void Awake()
+    {
+        server.MsgRecv += OnMsgRecv;
+        server.Start(port);
+    }
+
+    private void OnMsgRecv(object s, Server.ServerMsgEventArgs e)
+    {
+        Debug.Log("[" + e.Client.ToString() + "]: " + Encoding.ASCII.GetString(e.Buffer, 0, e.Len));
+
+        // Add client to connectionManager
+        //connectionManager.AddConnection(e.Client);
+
+        //server.Send(e.Client, e.Buffer, e.Len);
+    }
+
+    private void Update()
+    {
+        // Decrease KeepAlive and remove clients that have been inactive for too long
+        //connectionManager.Update();
+    }
+
+    private void OnApplicationQuit()
+    {
+        server.Stop();
+        server.MsgRecv -= OnMsgRecv;
+    }
+}
