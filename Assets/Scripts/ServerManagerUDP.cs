de14cf9525399c5b48cff1f9b2944357633e05dd
Alex Via <alexandre.via@i2cat.net>
2018-04-20 08:57:30 +0200

    Removed old ServerManagerUDP

--- Assets/Scripts/ServerManagerUDP.cs
+++ Assets/Scripts/ServerManagerUDP.cs
@@ -5,18 +5,12 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Sockets;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ServerManagerUDP : MonoBehaviour
+class ServerManagerUDP : MonoBehaviour
 {
-    private Socket socket;
-    private int packetRate = 60;
-
-    private System.Object clientsLock = new System.Object();
-    private List<ClientDataUDP> clients = new List<ClientDataUDP>();
+    private Server server = new ServerUDP();
 
     public List<Transform> spawnTransforms = new List<Transform>();
     private List<Trans> spawnTrans = new List<Trans>();
@@ -27,6 +21,8 @@ public class ServerManagerUDP : MonoBehaviour
 
     public bool benchmarkEnabled = false;
 
+    private ConnectionManager connectionManager = new ConnectionManager(100);
+
     private void Start()
     {
         // Fix the target framerate for standalone platforms
@@ -35,61 +31,51 @@ public class ServerManagerUDP : MonoBehaviour
         // Get spawn transforms so that we can access them from another thread
         spawnTrans = spawnTransforms.Select(x => new Trans(x.position, x.rotation, x.name)).ToList();
 
-        // Start listening
-        socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-        socket.Blocking = false;
-        socket.Bind(new IPEndPoint(IPAddress.Any, Constants.PORT));
-
         // Fetch the ball controller from the scene
         ballController = GameObject.Find(Constants.Ball).GetComponent<BallController>();
+
+        // Start the server
+        server.MsgRecv += OnMsgRecv;
+        server.Start(Constants.PORT);
+    }
+
+    private void OnApplicationQuit()
+    {
+        server.Stop();
+        server.MsgRecv -= OnMsgRecv;
     }
 
     private void Update()
     {
         HandleInput();
-        RemoveDisconnectedClients();
+        connectionManager.Tick();
 
-        byte[] data = new byte[socket.Available];
-        while(socket.Available > 0)
+        var clients = connectionManager.Connections;
+        foreach(var client in clients)
         {
-            try
-            {
-                EndPoint ep = new IPEndPoint(IPAddress.Any, 0);
-                int bytes_received = socket.ReceiveFrom(data, ref ep);
-                ClientDataUDP client = new ClientDataUDP((IPEndPoint)ep);
-                HandleClientPacket(client, data, bytes_received);
-            }
-            catch
-            {
+            client.clientData.Update();
 
-            }
-        }
-
-        foreach (var client in clients)
-        {
-            // Share transforms between Clients
-            if (clients.Count > 1)
+            if(clients.Count > 1)
             {
-                var others = clients.Where(x => !x.endPoint.Equals(client.endPoint)).ToList();
+                var others = clients.Where(x => !x.endPoint.Equals(client.endPoint) && x.clientData.TransformCount > 0).ToList();
                 //Packet packet = PacketBuilder.Build(Packet.PacketType.OtherClients, others);
                 // WORKAROUND, build the packet manually
                 List<byte> content = new List<byte>();
                 int size = Constants.HEADER_SIZE;
                 size += 1; // Number of clients (0-255)
-                foreach(var c in others)
+                foreach (var c in others)
                 {
-                    size += sizeof(int) + sizeof(byte) + c.TransformCount * Trans.Size;
+                    size += sizeof(int) + sizeof(byte) + c.clientData.TransformCount * Trans.Size;
                 }
                 content.AddRange(BitConverter.GetBytes((short)size));   // 2 bytes for the size
                 content.Add((byte)Packet.PacketType.OtherClients);      // 1 byte for the type
                 content.Add((byte)others.Count);
-                foreach(var c in others)
+                foreach (var c in others)
                 {
-                    content.AddRange(c.Serialize());
+                    content.AddRange(c.clientData.Serialize());
                 }
                 Packet packet = new Packet(content);
-
-                socket.SendTo(packet.ToArray(), client.endPoint);
+                server.Send(client.endPoint, packet.ToArray(), packet.Size);
             }
 
             // Send the objectsToSend if any
@@ -97,55 +83,50 @@ public class ServerManagerUDP : MonoBehaviour
             {
                 List<Trans> objects = objectsToSend.Select(x => new Trans(x.position, x.rotation, x.name)).ToList();
                 Packet packet = PacketBuilder.Build(Packet.PacketType.Objects, objects);
-                socket.SendTo(packet.ToArray(), client.endPoint);
+                server.Send(client.endPoint, packet.ToArray(), packet.Size);
             }
         }
     }
 
-    private void HandleClientPacket(ClientDataUDP client, byte[] data, int size)
+    private void OnMsgRecv(object sender, Server.ServerMsgEventArgs e)
     {
-        var existingClient = clients.Where(x => x.endPoint.Equals(client.endPoint)).FirstOrDefault();
-        if (existingClient == null)
+        if(!connectionManager.Contains(e.Client))
         {
-            clients.Add(client);
-
+            // Do this only if it is a new connection
             Packet p = PacketBuilder.Build(Packet.PacketType.Text, Constants.WelcomeMsg);
-            socket.SendTo(p.ToArray(), client.endPoint);
+            server.Send(e.Client, p.ToArray(), p.Size);
 
-            Trans spawn = spawnTrans[(clients.Count - 1) % spawnTrans.Count];
-            clients.Last().spawn = spawn;
+            Trans spawn = spawnTrans[connectionManager.Count % spawnTrans.Count];
             p = PacketBuilder.Build(Packet.PacketType.Spawn, spawn);
-            socket.SendTo(p.ToArray(), client.endPoint);
-
-            existingClient = client;
+            server.Send(e.Client, p.ToArray(), p.Size);
         }
-        existingClient.TTL = 100;
+        Connection connection = connectionManager.AddOrUpdateConnection(e.Client);
 
         int dataIndex = 0;
-        Packet packet = PacketBuilder.Parse(data, ref dataIndex);
+        Packet packet = PacketBuilder.Parse(e.Buffer, ref dataIndex);
         switch(packet.Type)
         {
             case Packet.PacketType.Text:
                 string text = ((PacketText)packet).Data;
-                if (text == Constants.ServeRequest)
+                if(text == Constants.ServeRequest)
                 {
                     Debug.Log("Serving Ball");
                     ballController.serve = true;
                 }
-                Debug.Log("[C(" + existingClient.endPoint + ")->S]: " + text
-                    + " (" + packet.Size + " of " + size + " bytes)");
-                foreach (var c in clients)
+                Debug.Log("[C(" + e.Client + ")->S]: " + text
+                    + " (" + packet.Size + " of " + e.Len + " bytes)");
+                foreach (var c in connectionManager.Connections)
                 {
-                    if (c.endPoint == existingClient.endPoint)
+                    if (c.endPoint == e.Client)
                         continue;
 
-                    socket.SendTo(packet.ToArray(), c.endPoint);
+                    server.Send(c.endPoint, packet.ToArray(), packet.Size);
                 }
                 break;
 
             case Packet.PacketType.Sensors:
                 List<Trans> transforms = ((PacketSensors)packet).Data;
-                existingClient.Update(transforms);
+                connection.clientData.Update(transforms);
                 break;
 
             case Packet.PacketType.Benchmark:
@@ -155,45 +136,24 @@ public class ServerManagerUDP : MonoBehaviour
 
             default:
                 Debug.Assert(false);
-                Debug.LogError("Invalid PacketType" + " (" + packet.Size + " of " + size + " bytes)");
+                Debug.LogError("Invalid PacketType" + " (" + packet.Size + " of " + e.Len + " bytes)");
                 break;
         }
     }
+
 
     private void HandleInput()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Debug.Log(clients.Count + " clients online");
-            //foreach (var client in clients)
-            //{
-            //    Debug.Log(client.socket.RemoteEndPoint.ToString());
-            //    Debug.Log(client.TransformCount + " transforms");
-            //    foreach (var key in client.TransformKeys)
-            //    {
-            //        Debug.Log("Key: " + key + " Id: " + client.GetTransform(key).Id);
-            //    }
-            //}
+            Debug.Log(connectionManager.Count + " clients online");
         }
 
         // Benchmarks
-        if (benchmarkEnabled && clients.Count > 0)
+        if (benchmarkEnabled && connectionManager.Count > 0)
         {
             Packet packet = PacketBuilder.Build(Packet.PacketType.Benchmark, new NetBenchmarks());
-            socket.SendTo(packet.ToArray(), clients.First().endPoint);
-        }
-    }
-
-    private void RemoveDisconnectedClients()
-    {
-        for (int i = clients.Count - 1; i >= 0; i--)
-        {
-            --clients[i].TTL;
-            if (clients[i].TTL < 0)
-            {
-                clients[i].Destroy();
-                clients.Remove(clients[i]);
-            }
+            server.Send(connectionManager.Connections.First().endPoint, packet.ToArray(), packet.Size);
         }
     }
 
@@ -205,12 +165,9 @@ public class ServerManagerUDP : MonoBehaviour
         Packet packet = PacketBuilder.Build(Packet.PacketType.Text, text);
 
         // Send the packet to all the connected clients
-        lock (clientsLock)
+        foreach (var client in connectionManager.Connections)
         {
-            foreach (var client in clients)
-            {
-                socket.SendTo(packet.ToArray(), client.endPoint);
-            }
+            server.Send(client.endPoint, packet.ToArray(), packet.Size);
         }
     }
 }
