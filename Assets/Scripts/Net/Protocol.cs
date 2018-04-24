914174eaa83b95fea58be06360bf02710e8f7269
Alex Via <alexandre.via@i2cat.net>
2018-04-24 15:34:08 +0200

    Added Protocol enumeration

---
+++ Assets/Scripts/Net/Protocol.cs
@@ -0,0 +1,12 @@
+﻿// Licensed under the LGPL 3.0
+// See the LICENSE file in the project root for more information.
+// Author: alexandre.via@i2cat.net
+
+namespace Net
+{
+    public enum Protocol
+    {
+        Tcp,
+        Udp
+    }
+}
