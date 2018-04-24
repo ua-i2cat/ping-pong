127e4bb7197555ea5abf543eff7de651891d242d
Alex Via <alexandre.via@i2cat.net>
2018-04-24 15:35:56 +0200

    Added factories to create servers and clients

---
+++ Assets/Scripts/Net/ClientFactory.cs
@@ -0,0 +1,24 @@
+﻿// Licensed under the LGPL 3.0
+// See the LICENSE file in the project root for more information.
+// Author: alexandre.via@i2cat.net
+
+using System;
+
+namespace Net
+{
+    public static class ClientFactory
+    {
+        public static Client Create(Protocol protocol)
+        {
+            switch(protocol)
+            {
+                case Protocol.Tcp:
+                    return new ClientTCP();
+                case Protocol.Udp:
+                    return new ClientUDP();
+                default:
+                    throw new InvalidOperationException();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Net/ServerFactory.cs
@@ -0,0 +1,24 @@
+﻿// Licensed under the LGPL 3.0
+// See the LICENSE file in the project root for more information.
+// Author: alexandre.via@i2cat.net
+
+using System;
+
+namespace Net
+{
+    public static class ServerFactory
+    {
+        public static Server Create(Protocol protocol)
+        {
+            switch(protocol)
+            {
+                case Protocol.Tcp:
+                    return new ServerTCP();
+                case Protocol.Udp:
+                    return new ServerUDP();
+                default:
+                    throw new InvalidOperationException();
+            }
+        }
+    }
+}
